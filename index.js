c76abf592e9f3743b1f5eb1576de7663ad1c9287
Danny Gleckler <daniel.gleckler@gmail.com>
2016-05-13 01:27:01 -0400

    Update to ifraung

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
-angular.module('angular-ifrau', [])
+angular.module('ifraung', [])
 
 .provider('AppHost', function AppHostProvider() {
 
-	var conf = {};
 	var client;
 	var connected = false;
 
-	function init(_client) {
+	this.init = function(_client) {
 		client = _client;
-	}
-
-	this.init = init;
+	};
 
 	this.$get = function AppHostFactory() {
 
 		function AppHost() {
 
 			function connect() {
-				var p = client.connect();
-				connected = true;
-				return p;
-			}
-
-			function sendEvent(eventName, payload) {
-				client.sendEvent(eventName, payload);
+				return client.connect().then(function() {
+					connected = true;
+				});
 			}
 
 			function sendMessage(level, message) {
@@ -33,25 +26,12 @@ angular.module('angular-ifrau', [])
 				});
 			}
 
-			function request(type, options) {
-				return client.request(type, options);
-			}
-
-			function navigate(url) {
-				client.navigate(url);
-			}
-
-			function getService(serviceType, version) {
-				return client.getService(serviceType, version);
-			}
-
-			this.sendEvent = sendEvent;
+			this.sendEvent = client.sendEvent;
 			this.sendMessage = sendMessage;
-			this.conf = conf;
-			this.navigate = navigate;
-			this.connect = connect;
-			this.request = request;
-			this.getService = getService;
+			this.navigate = client.navigate;
+			this.connect = client.connect;
+			this.request = client.request;
+			this.getService = client.getService;
 		}
 
 		return new AppHost();
